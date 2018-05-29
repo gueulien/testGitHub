0135e0906e51e06c3e38387f8be6314e1b44445f
Julien Pringuay <julien@iMac-de-Julien.local>
2018-05-29 23:19:47 +0200

    Initial Commit

---
+++ p2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  p2
+//
+//  Created by Julien Pringuay on 29/05/2018.
+//  Copyright © 2018 Julien Pringuay. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
