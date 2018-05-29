846858e81b51e1aed7078526791029719a37a162
Julien Pringuay <julien@iMac-de-Julien.local>
2018-05-29 22:51:22 +0200

    commit 4

--- testGitHub/main.cpp
+++ testGitHub/main.cpp
@@ -1,17 +1,1 @@
-//
-//  main.cpp
-//  testGitHub
-//
-//// bonjout
-//  Created by Julien Pringuay on 29/05/2018.
-//  Copyright © 2018 Julien Pringuay. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    
-    // insert code here...
-    std::cout << "Hello, Worldo!\n";
-    return 0;
-}
+coucou
